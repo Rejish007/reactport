b8b08037f7c91e48a108861647bc9bfcd0ea0584
Rejish007 <61198869+Rejish007@users.noreply.github.com>
2024-07-07 22:40:12 +0545

    jhkhk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import DisplaySec from './pages/home/DisplaySec'
+import Tech from './pages/home/Tech'
+import Intro from './pages/home/Intro'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <DisplaySec />
+      <Tech />
+      <Intro />
+      <Footer />
+
+
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
