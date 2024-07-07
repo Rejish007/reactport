e5d3abdd61430b704a360444b949714d919d6f86
Rejish007 <61198869+Rejish007@users.noreply.github.com>
2024-07-07 23:45:09 +0545

    flksdnf

---
+++ src/pages/home/DisplaySec.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const DisplaySec = () => {
+  return (
+    <div className='grid grid-cols-2 items-center sm:grid-cols-1 sm:mb-11'>
+
+
+      <div className="dis-img h-[360px]">
+        <dotlottie-player src="https://lottie.host/3ad97a6b-7ae2-4c5f-89cb-7118cbb5c434/0awMBNx3en.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
+
+
+      </div>
+
+      <div className="dis-info space-y-2 sm:text-center sm:px-4">
+        <h1 className='text-3xl '>Hi, I'm Rejish</h1>
+        <p className='text-[#B061DF] text-xl'>Some Dev,Freelancer,Rounder</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, sit molestiae optio possimus maxime vero mollitia blanditiis quaerat quis nostrum, sed quae fugit unde temporibus earum perferendis expedita ab nulla Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, a iure blanditiis tempore illo amet maiores eos est nulla. Officia ipsum vero consequuntur nobis molestias, voluptates saepe laboriosam doloribus earum..</p>
+
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default DisplaySec
